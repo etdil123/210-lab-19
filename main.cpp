5d83ee929f891d96e798af52539da6b5ab472057
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-10-06 21:00:33 -0700

    10 min commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,32 @@
 #include <string>
 using namespace std;
 
+class Movie {
+
+    private:
+        string movieTitle;
+        // two separate structs to store linked lists for ratings and comments
+        struct RatingsNode {
+            float movieRating;
+            RatingsNode * nextRating;
+        };
+
+        struct CommentsNode {
+            string movieComments;
+            CommentsNode * nextComment;
+        };
+
+    // head variables to know where to start linked list
+    RatingsNode *ratingsHead;
+    CommentsNode *commentsHead;
+
+
+};
+
+
 
 int main() {
+
 
 
 
